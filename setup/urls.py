c7d6dd629cdd530516e7919a5beaebea683ffd8a
Mateus Melo <mateus.melo@fpf.br>
2024-07-16 18:03:47 -0400

    Projeto de estudo de Django Rest Framework

---
+++ setup/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+from escola.views import AlunosViewSet, CursosViewSet, ListaAlunosMatriculados, ListaMatriculasAluno, MatriculaViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
+    path('curso/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view()),
+]
